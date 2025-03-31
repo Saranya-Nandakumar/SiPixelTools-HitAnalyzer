0613a068f82319ef3ae76fc1fa784327519b794a
Henriette <henriette.aarup.petersen@cern.ch>
2025-03-31 23:05:20 +0200

    add digi to digi script with morphing enabled

---
+++ scripts/runDigiToDigiMorph.py
@@ -0,0 +1,53 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Eras.Era_Run3_2024_cff import Run3_2024
+#from Configuration.ProcessModifiers.siPixelDigiMorphing_cff import siPixelDigiMorphing
+#process = cms.Process("DigiToRaw1",Run3_2024,siPixelDigiMorphing)
+process = cms.Process("DigiToRaw1",Run3_2024)                                                                                                                                                              
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.load("Configuration.StandardSequences.GeometryDB_cff")
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '142X_mcRun3_2025_realistic_v5', '')
+
+process.load("EventFilter.SiPixelRawToDigi.SiPixelDigiToRaw_cfi")
+process.load("EventFilter.SiPixelRawToDigi.SiPixelRawToDigi_cfi")
+
+# for simultaions                                                                                                                                                                                           
+process.siPixelDigis.InputLabel = 'siPixelRawData'
+process.load("RecoLocalTracker.SiPixelDigiReProducers.siPixelDigisMorphed_cfi")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100))
+
+process.source = cms.Source("PoolSource",
+  fileNames =  cms.untracked.vstring(
+  'file:step2.root'
+ )
+)
+
+process.MessageLogger = cms.Service("MessageLogger",
+    debugModules = cms.untracked.vstring('siPixelRawData'),
+    #destinations = cms.untracked.vstring('log'),
+    #log = cms.untracked.PSet( threshold = cms.untracked.string('WARNING'))
+)
+
+process.out = cms.OutputModule("PoolOutputModule",
+    fileName =  cms.untracked.string('file:digitFromRawMorph.root'),
+   outputCommands = cms.untracked.vstring("drop *","keep *_siPixelDigis_*_*")
+)
+
+process.a = cms.EDAnalyzer("PixDigisTest",
+    Verbosity = cms.untracked.bool(False),
+    phase1 = cms.untracked.bool(True),
+    #src = cms.InputTag("siPixelDigis"),
+    src = cms.InputTag("siPixelDigisMorphed"),
+)
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('digis_histos_morph.root')
+)
+
+process.p = cms.Path(process.siPixelRawData*process.siPixelDigis*process.siPixelDigisMorphed*process.a)
+#process.p = cms.Path(process.siPixelRawData*process.siPixelDigis*process.a)
+process.ep = cms.EndPath(process.out)
